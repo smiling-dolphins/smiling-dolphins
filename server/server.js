a56b3760b44f96806b35a946fc08e510c4f103b1
Ksenia Smith <smithkseniav@gmail.com>
2015-09-29 14:58:31 -0700

    [comments] pseudocoded routes

--- server/server.js
+++ server/server.js
@@ -1,1 +1,20 @@
 var db = require('../db/config');
+
+// USERS: 
+
+  // GET 1 user (with trips)
+
+  // POST 1 user
+
+
+// TRIPS:
+
+  // GET all trips (with users)
+
+  // GET 1 trip (with photos)
+
+  // POST 1 trip:
+    // call instafeed
+    // save tag(trip name) & photos(with user & trip IDs) data to our DB
+    // respond with OK
+    // return trip/photos data ???
