b4efe685f44273e3dd96e424fe53939c3617f5ea
Ksenia Smith <smithkseniav@gmail.com>
2015-09-29 18:55:22 -0700

    [save] made basic server to fetch static files

--- server/server.js
+++ server/server.js
@@ -1,4 +1,21 @@
 var db = require('../db/config');
+var http = require('http');
+var express = require('express');
+var Promise = require('bluebird');
+
+var app = express();
+
+app.use(express.static(__dirname + '/../client'));
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.get('/', function(req, res){
+  res.sendFile('/index.html');
+});
 
 // USERS: 
 
@@ -18,3 +35,5 @@ var db = require('../db/config');
     // save tag(trip name) & photos(with user & trip IDs) data to our DB
     // respond with OK
     // return trip/photos data ???
+app.listen(process.env.PORT || 3000);
+console.log("Listening...")
