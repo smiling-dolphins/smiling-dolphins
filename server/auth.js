86e41e760c79787ac79bb2d1a9b0fb440f82c717
ChrisC2 <chrisjc790@gmail.com>
2015-09-29 20:08:26 -0700

    [ADD] OAuth Passport & Server Build In Progress

---
+++ server/auth.js
@@ -0,0 +1,5 @@
+var Auth = {
+  clientId: "a512c8a691474832ac065ff22317d8c1",
+  clientSecret: "22b73d18bf064621861185da1ba65945"
+  accessToken: "22125417.d904cd4.44abd06ef59d43e5b0fc7e9b4f347ebb"
+}
--- server/server.js
+++ server/server.js
@@ -1,6 +1,55 @@
 var db = require('../db/config');
+var express = require('express');
+var bodyParser = require('body-parser');
+var User = require('..db/models/user');
+var Trip = require("..db/models/trip");
+var Photo = require("..db/models/photo");
+var passport = require("passport");
 
-// USERS: 
+var app = express();
+app.use(bodyParser.json());
+
+require("/passport.js")(passport)
+
+//Required for Passport
+app.use(express.session({ secret: 'Frowning Dolphins' }));
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(express.static(__dirname + "/client"));
+
+app.get("/", function (req, res) {
+// Handle the Index Page
+// Pull all Trip data of all users w/ associated photos from db to post on page
+
+})
+//Direct to Instagram Login
+app.get('/auth/instagram',
+  passport.authenticate('instagram'));
+
+//Redirect Back to Home Page upon Authentication
+app.get('/auth/instagram/callback',
+  passport.authenticate('instagram', { failureRedirect: '/login' }),
+  function(req, res) {
+    res.redirect('/');
+  });
+
+app.post('/api/trip', function (req, res) {
+  var tripName = req.body.name;
+  var newTrip = Trip();
+  newTrip.name = tripName;
+  //create a new trip & send Instafeed post request for user
+  // filter for newTrip tag we just created
+  // add photos to the db with associated trip Id
+
+
+
+
+});
+
+app.listen(8000);
+console.log("listening on Port 8000...")
+// USERS:
 
   // GET 1 user (with trips)
 
